7c2633597611a9236221a13e551119153822c5b7
Olesiy <olesiytrupoedova@gmail.com>
2024-03-10 11:14:07 +0300

    dev

--- Func/ex1.c
+++ Func/ex1.c
@@ -16,6 +16,8 @@ void search_abonent(abonent mass[MAX_SIZE_MASS]);
 void print_abonent(abonent mass[MAX_SIZE_MASS]);
 void del_abonent(abonent mass[MAX_SIZE_MASS]);
 
+void clear_enter(abonent *a);
+
 int main(){
     abonent mass[MAX_SIZE_MASS] = {0};
     int argument = 0;
@@ -23,15 +25,33 @@ int main(){
         argument = menu_bar();
         if(argument == 1){
             new_abonent(mass);
-        } else if(argument == 2){
+        } 
+        if(argument == 2){
             del_abonent(mass); 
-        } else if(argument == 3){
+        } 
+        if(argument == 3){
             search_abonent(mass);
-        } else if(argument == 4){
+        }  
+        if(argument == 4){
             print_abonent(mass);
-        } else if(argument == 6){
+        } 
+        if(argument == 6){
             printf("ERROR\n");
             break;
+        }
+    }
+}
+
+void clear_enter(abonent *a){
+    for(int i = 0; i <  SIZE_ELEMENTS; i++){
+        if(a->name[i] == '\n'){
+            a->name[i] = 0;
+        }
+        if(a->second_name[i] == '\n'){
+            a->second_name[i] = 0;
+        }
+        if(a->tell[i] == '\n'){
+            a->tell[i] = 0;
         }
     }
 }
@@ -39,13 +59,22 @@ int main(){
 void print_abonent(abonent mass[MAX_SIZE_MASS]){
     for(short i = 0; i < MAX_SIZE_MASS; i++){
         if(mass[i].name[0]){
-            printf("%s %s %s\n", mass[i].name, mass[i].second_name, mass[i].tell);
+            printf("name: %s\n"
+                    "second name: %s\n"
+                    "call: %s\n\n", mass[i].name, mass[i].second_name, mass[i].tell);
+        }
     }
-}}
+}
 
 void del_abonent(abonent mass[MAX_SIZE_MASS]){
     char del_name[SIZE_ELEMENTS] = {0};
     fgets(del_name, 10, stdin);
+    for(short i = 0; i < SIZE_ELEMENTS;i++){
+        if(del_name[i] == '\n'){
+            del_name[i] = 0;
+            break;
+        }
+    }
     fflush(stdin);
     for(short i = 0; i < SIZE_ELEMENTS; i++){
         if(strcmp(mass[i].name, del_name) == 0){
@@ -60,20 +89,28 @@ void search_abonent(abonent mass[MAX_SIZE_MASS]){
     char search_name[SIZE_ELEMENTS] = {0};
     fgets(search_name, 10, stdin);
     fflush(stdin);
+    for(short i = 0; i < SIZE_ELEMENTS;i++){
+        if(search_name[i] == '\n'){
+            search_name[i] = 0;
+            break;
+        }
+    }
     for(short i = 0; i < SIZE_ELEMENTS; i++){
         if(strcmp(mass[i].name, search_name) == 0){
-            printf("%s%s%s\n", mass[i].name, mass[i].second_name, mass[i].tell);
+            printf("name: %s\n"
+                    "second name: %s\n"
+                    "call: %s\n", mass[i].name, mass[i].second_name, mass[i].tell);
         }
     }
 }
 
 int menu_bar(){
     int menu = 0;
-    printf("1. new abonent\n");
-    printf("2. del abonent\n");
-    printf("3. search abonent\n");
-    printf("4. print abonent\n");
-    printf("5. exit\n");  
+    printf("1. new abonent\n"
+            "2. del abonent\n"
+            "3. search abonent\n"
+            "4. print abonent\n"
+            "5. exit\n");  
     if(scanf("%d", &menu) != 1){
         menu = 6;
     }
@@ -83,7 +120,7 @@ int menu_bar(){
 
 void new_abonent(abonent mass[MAX_SIZE_MASS]){
     short i = 0;
-    for(; i < MAX_SIZE_MASS; i++){
+    for(i = 0; i < MAX_SIZE_MASS; i++){
         if(mass[i].name[0] == 0) break;
     }
     if(i == 99 && mass[i].name[0]){
@@ -95,5 +132,6 @@ void new_abonent(abonent mass[MAX_SIZE_MASS]){
         fflush(stdin);
         fgets(mass[i].tell, 10, stdin);
         fflush(stdin);
+        clear_enter(&mass[i]);
     }
 }
