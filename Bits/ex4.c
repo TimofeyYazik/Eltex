d751dba6d969df21b45021a5d1e433ba20a945f8
Olesiy <olesiytrupoedova@gmail.com>
2024-03-17 07:22:18 +0300

    q

--- Bits/ex4.c
+++ Bits/ex4.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+#define MASK 0xFF00FFFF
+#define MASK2 0x00FF0000
+
 int main(){
     int res = 0;
+    int byte = 0;
     scanf("%d", &res);
-    res <<= 16;
+    scanf("%d", &byte);
+    byte <<= 16;
+    byte |= MASK;
+    res &= byte;
+    byte &= MASK2;
+    res |= byte;
     printf("%d\n", res);
 }
