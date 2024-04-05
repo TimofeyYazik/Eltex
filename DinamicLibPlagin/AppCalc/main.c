6d090b8ea28c8e361cfc17093d945b1cf821f995
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-05 14:16:35 +0300

    dev

--- DinamicLibPlagin/AppCalc/main.c
+++ DinamicLibPlagin/AppCalc/main.c
@@ -12,6 +12,7 @@ int main(int argc, char **argv){
   int count_func_arr = 0;
   int (**func_calc_int)(int, int) = NULL;
   int var_func_one = 0, var_func_two = 0;
+  int result = 0;
   volatile int argument = 1;
   service_info service = {0};
   ParseFlags(&service, argc, argv);
@@ -20,7 +21,8 @@ int main(int argc, char **argv){
     argument = Menu(service);
     if (argument > 0 && argument < count_func_arr) {
       scanf("%d %d", &var_func_one, &var_func_two);
-      func_calc_int[argument - 1](var_func_one, var_func_two);
+      result = func_calc_int[argument - 1](var_func_one, var_func_two);
+      printf("%d\n", result);
     } else {
       if(!argument) continue;
       printf("not this argument");
