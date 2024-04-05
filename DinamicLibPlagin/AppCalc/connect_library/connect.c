3e71776b365c891597d9687d6a14c40c571e26dc
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-05 14:30:51 +0300

    def

--- DinamicLibPlagin/AppCalc/connect_library/connect.c
+++ DinamicLibPlagin/AppCalc/connect_library/connect.c
@@ -21,12 +21,12 @@ void ConnectLib(int (**func_calc_int)(int, int),int *count, const service_info s
         break;
       }
       for(int l = 0; l < *count_func; l++){
-        if(!strcmp(service.func[j], *(func_name)[l])){
+        if(!strcmp(service.func[j], (*func_name)[l])){
           func_to_be = 1;
         }
       }
       if(func_to_be){
-        *count++;
+        (*count)++;
         func_calc_int = realloc(func_calc_int, (*count * sizeof(int (*)(int, int))));
         func_calc_int[*count - 1] = dlsym(handler_lib, service.func[j]);
       }
