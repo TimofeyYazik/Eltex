666470033dfcaada6a249d24ff98dc3ac685eee9
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-05 13:29:01 +0300

    dev

--- DinamicLibPlagin/AppCalc/main.c
+++ DinamicLibPlagin/AppCalc/main.c
@@ -17,13 +17,12 @@ int main(int argc, char **argv){
 
   int var_func_one = 0, var_func_two = 0;
   volatile int argument = 1;
-  char ***func_name = NULL;
   service_info service = {0};
   ParseFlags(&service, argc, argv);
   ConnectLib(func_calc_int, &count_func_arr, service);
   while (argument) {
     argument = Menu(service);
-    if (argument > 0 && argument < count_func) {
+    if (argument > 0 && argument < count_func_arr) {
       scanf("%d %d", &var_func_one, &var_func_two);
       func_calc_int[argument - 1](var_func_one, var_func_two);
     } else {
