c49b1e5b48f2ed4d2c655ee38e76fc42fe852c09
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-05 14:33:22 +0300

    dev

--- DinamicLibPlagin/AppCalc/connect_library/connect.c
+++ DinamicLibPlagin/AppCalc/connect_library/connect.c
@@ -10,12 +10,13 @@ void ConnectLib(int (**func_calc_int)(int, int),int *count, const service_info s
   int func_to_be = 0;
   for(int i = 0; i < service.lib_count; i++){
     void *handler_lib = dlopen(service.lib[i], RTLD_LAZY);
-    int *count_func = dlsym(handler_lib, "g_number_functions");
-    char ***func_name = dlsym(handler_lib, "g_name_func");
     if(handler_lib == NULL){
       fprintf(stderr, "path not found");
       exit(EXIT_FAILURE);
     }
+    int *count_func = dlsym(handler_lib, "g_number_functions");
+    char ***func_name = dlsym(handler_lib, "g_name_func");
+    printf("%d\n", *count_func);
     for(int j = 0; j < service.func_count; j++){
       if(!strcmp(service.func[j], "new lib")) {
         break;
