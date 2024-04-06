f8b542bc8b0ddd8c62349d07b41373eafc924635
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-06 09:31:21 +0300

    dbg

--- DinamicLibPlagin/AppCalc/connect_library/connect.c
+++ DinamicLibPlagin/AppCalc/connect_library/connect.c
@@ -7,7 +7,7 @@
 #include "connect.h"
 
 void ConnectLib(int (***func_calc_int)(int, int), handler_t *handler, int *count, const service_info service){
-  int func_to_be = 0;
+  // int func_to_be = 0;
   int j = 0;
   for(int i = 0; i < service.lib_count; i++){
     handler->len++;
@@ -17,29 +17,29 @@ void ConnectLib(int (***func_calc_int)(int, int), handler_t *handler, int *count
       fprintf(stderr, "path not found");
       exit(EXIT_FAILURE);
     }
-    int *count_func = dlsym(handler->handler_arr[i], "g_number_functions");
-    if(count_func == NULL){
-      fprintf(stderr, "var not found");
-      exit(EXIT_FAILURE);
-    }
-    char ***func_name = dlsym(handler->handler_arr[i], "g_name_func");
+    // int *count_func = dlsym(handler->handler_arr[i], "g_number_functions");
+    // if(count_func == NULL){
+    //   fprintf(stderr, "var not found");
+    //   exit(EXIT_FAILURE);
+    // }
+    // char ***func_name = dlsym(handler->handler_arr[i], "g_name_func");
     // char **func_name = *func_name_;
-    if(func_name == NULL){
-      fprintf(stderr, "var not found");
-      exit(EXIT_FAILURE);
-    }
-    printf("%s\n", (*func_name)[0]);
+    // if(func_name == NULL){
+    //   fprintf(stderr, "var not found");
+    //   exit(EXIT_FAILURE);
+    // } 
+    // printf("%s\n", (*func_name)[0]); //в этой строке происходит seg fault 
     for(; j < service.func_count; j++){
       if(!strcmp(service.func[j], "new lib")) {
         j++;
         break;
       }
-      for(int l = 0; l < *count_func; l++){
-        if(!strcmp(service.func[j], (*func_name)[l])){
-          func_to_be = 1;
-        }
-      }
-      if(func_to_be){
+      // for(int l = 0; l < *count_func; l++){
+      //   if(!strcmp(service.func[j], (*func_name)[l])){
+      //     func_to_be = 1;
+      //   }
+      // }
+      // if(func_to_be){
         (*count)++;
         (*func_calc_int) = realloc(*func_calc_int, (*count * sizeof(int (*)(int, int))));
         (*func_calc_int)[*count - 1] = dlsym(handler->handler_arr[i], service.func[j]);
@@ -47,8 +47,8 @@ void ConnectLib(int (***func_calc_int)(int, int), handler_t *handler, int *count
           fprintf(stderr, "not function\n");
           exit(EXIT_FAILURE);
         }
-      }
-      func_to_be = 0;
+      // }
+      // func_to_be = 0;
     }
   }
 }
