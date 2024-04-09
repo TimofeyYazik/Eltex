eb2dbe5b0cce1c8dd6f73c851406eabe25dc5764
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-09 17:38:19 +0300

    dev

--- FileManager/main.c
+++ FileManager/main.c
@@ -3,24 +3,37 @@
 #include <fcntl.h>          /* Definition of AT_* constants */
 #include <dirent.h>
 #include <sys/types.h>
+// #include <stdint.h>
 #include <errno.h>
 
 #define MAX_LEN_NAME_FILE 255
 
 int main(){
   int counter = 0;
-  DIR *use_dir = opendir("../");
-  struct dirent *namelist[100] = {NULL};
-  struct dirent *buff;
-  while((buff = readdir(use_dir)) != NULL){
-    namelist[counter] = buff;
-    counter++;
+  // DIR *use_dir = opendir("../");
+  struct dirent **namelist;
+  // struct dirent *buff;
+  // while((buff = readdir(use_dir)) != NULL){
+  //   namelist[counter] = buff;
+  //   counter++;
+  // }
+  int driver = 0;
+  int len_namelist = scandir(".", &namelist, NULL, alphasort);
+  while(driver != -1){
+    for(int i = 0; i < len_namelist; i++){
+      printf("%d) %s\n", i ,namelist[i]->d_name);
+    }
+    scanf("%d", driver);
+    switch_dir(namelist[i]->d_name);
   }
-  
-  for(int i = 0; i < counter; i++){
-    printf("%s\n", namelist[i]->d_name);
-  }
-  
-  closedir(use_dir);
+  // closedir(use_dir);
   exit(EXIT_SUCCESS);
+}
+
+
+void switch_dir(char name[256]){
+  DIR* use_dir = opendir(name);
+  long ds = telldir(use_dir);
+  seekdir(use_dir, ds);
+  closedir(use_dir);
 }
