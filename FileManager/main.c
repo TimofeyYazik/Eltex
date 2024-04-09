e2f9d6570268da31e9313bdafd5682d4e24f3227
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-09 16:43:00 +0300

    dev

---
+++ FileManager/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>          /* Definition of AT_* constants */
+#include <dirent.h>
+#include <sys/types.h>
+#include <errno>
+
+
+int main(){
+  //DIR *use_dir = opendir("../");
+  struct dirent **namelist = NULL;
+  
+  int count_line = scandir("../", &namelist, NULL, &alphasort());
+  for(int i = 0; i < count_line; i++){
+    printf("%s\n", namelist[i]->d_name);
+  }
+  free(namelist);
+
+  exit(EXIT_SUCCESS);
+}
