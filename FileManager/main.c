a63f06eb68191c1c67e435035d40338af642f7b9
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-09 18:28:18 +0300

    dev

--- FileManager/main.c
+++ FileManager/main.c
@@ -1,45 +1,57 @@
+
 #include <dirent.h>
 #include <fcntl.h> /* Definition of AT_* constants */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-// #include <stdint.h>
 #include <errno.h>
 
-// #define MAX_LEN_NAME_FILE 256
+#define MAX_ENTRIES 100
 
 void QuickSort(struct dirent *namelist[], int start, int finish);
 
 int main() {
   int counter = 0;
-  // DIR *use_dir = opendir("../");
-  struct dirent *namelist[100];
+  struct dirent *namelist[MAX_ENTRIES];
 
   DIR *use_dir = opendir(".");
-  int driver = 0;
+  if (use_dir == NULL) {
+    perror("Error opening current directory");
+    exit(EXIT_FAILURE);
+  }
+
   while (1) {
     counter = 0;
     struct dirent *buff;
     while ((buff = readdir(use_dir)) != NULL) {
       namelist[counter] = buff;
       counter++;
+      if (counter >= MAX_ENTRIES) break; // Prevent buffer overflow
     }
+
     QuickSort(namelist, 0, counter - 1);
     for (int i = 0; i < counter; i++) {
       printf("%d) %s\n", i, namelist[i]->d_name);
     }
+
+    printf("Enter the index of the directory you want to enter, or -1 to exit: ");
+    int driver;
     scanf("%d", &driver);
     printf("\n\n\n");
-    if (driver >= counter || driver < 0) break;
+    if (driver < 0 || driver >= counter) break;
     closedir(use_dir);
     use_dir = opendir(namelist[driver]->d_name);
-    if(use_dir == NULL){
-      printf("PIZDA\n");
+    if (use_dir == NULL) {
+      perror("Error opening selected directory");
       exit(EXIT_FAILURE);
     }
-    long ds = telldir(use_dir);
-    seekdir(use_dir, ds);
+    long td = telldir(use_dir);
+    if(td == -1){
+      exit(EXIT_FAILURE);
+    }
+    seekdir(use_dir, td);
   }
+
   if (use_dir != NULL) closedir(use_dir);
   exit(EXIT_SUCCESS);
 }
@@ -50,10 +62,11 @@ void QuickSort(struct dirent *namelist[], int start, int finish) {
   int middle = (start + finish) / 2;
   char pivot = namelist[middle]->d_name[0];  
 
-  // Разделение массива на две части
   while (left <= right) {
     while (namelist[left]->d_name[0] < pivot) left++;
-    while (namelist[right]->d_name[0] > pivot) right--;
+    while (namelist[right]->d_name[0] > pivot)
+
+ right--;
 
     if (left <= right) {
       struct dirent *buff = namelist[left];
