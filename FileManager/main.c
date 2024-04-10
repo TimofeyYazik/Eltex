33fb377c0035404d5df161b1978451513c8f84d6
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-10 17:45:18 +0300

    dev

--- FileManager/main.c
+++ FileManager/main.c
@@ -1,4 +1,3 @@
-// #include <curses.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
@@ -14,14 +13,28 @@
 #define TRUE 1
 #define FALSE 0
 
-void VimCall(char *name){
-  int status = 0;
-  pid_t pid_vim = fork();
-  if(!pid_vim){
-    execl("/usr/bin/vim", "vim", name, NULL);
-  }
-  pid_vim = wait(&status);
+void saveTerminalSettings(struct termios *original_termios) {
+    tcgetattr(STDIN_FILENO, original_termios);
 }
+
+void restoreTerminalSettings(const struct termios *original_termios) {
+    tcsetattr(STDIN_FILENO, TCSANOW, original_termios);
+}
+
+void VimCall(char *name) {
+    struct termios original_termios;
+    saveTerminalSettings(&original_termios);
+
+    int status = 0;
+    pid_t pid_vim = fork();
+    if (!pid_vim) {
+        execl("/usr/bin/vim", "vim", name, NULL);
+    }
+    pid_vim = wait(&status);
+
+    restoreTerminalSettings(&original_termios);
+}
+
 
 void draw_buttons(struct dirent **namelist_dir, int count, int selected) {
   clear();  // Очистить экран
