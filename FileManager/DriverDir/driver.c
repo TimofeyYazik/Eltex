7797471c647c9614837e4724392455f0471807f3
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-10 12:30:05 +0300

    dev

---
+++ FileManager/DriverDir/driver.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+int DriverDir(int *len_namelist, struct dirent **namelist_dir, char *name_dir) {
+  int error = 0;
+  if(strcmd(name_dir, ".") != 0){
+    error = chdir(name_dir);
+    // perror("Not dir:");
+  }  
+  if(error != -1){
+    *len_namelist = scandir(".", namelist_dir, NULL, alphasort);
+  }
+
+  return error;
+}
+
--- FileManager/main.c
+++ FileManager/main.c
@@ -1,60 +1,39 @@
-
-#include <dirent.h>
-#include <fcntl.h> /* Definition of AT_* constants */
-#include <stdio.h>
+#include <curses.h>
+#include <signal.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <unistd.h>
+#include <sys/ioctl.h>
+#include <termios.h>
 
-#define MAX_ENTRIES 100
+#define MAX_NAME_LEN 15
 
-void QuickSort(struct dirent *namelist[], int start, int finish);
-
-int main() {
-  int len_namelist = 0;
-  struct dirent **namelist;
-  int ch = 0;
-  while (1)
-  {
-    len_namelist = scandir(".", &namelist, NULL, alphasort);
-    for (int i = 0; i < len_namelist; i++) {
-      printf("%d) %s\n", i, namelist[i]->d_name);
-    }
-    printf("Enter the index of the directory you want to enter, or -1 to exit: ");
-    int driver;
-    scanf("%d", &driver);
-    if (driver < 0 || driver >= len_namelist) break;
-    printf("\n\n\n");
-    chdir(namelist[driver]->d_name);
-    free(namelist);
-  }
-  free(namelist);
-
+void sig_winch(int signo) {
+  struct winsize size;
+  ioctl(fileno(stdout), TIOCGWINSZ, (char *)&size);
+  resizeterm(size.ws_row, size.ws_col);
+}
+int main(int argc, char **argv) {
+  WINDOW *wnd;
+  char name[MAX_NAME_LEN + 1];
+  initscr();
+  signal(SIGWINCH, sig_winch);
+  curs_set(TRUE);
+  start_color();
+  refresh();
+  init_pair(1, COLOR_YELLOW, COLOR_BLUE);
+  wnd = newwin(5, 23, 2, 2);
+  wbkgd(wnd, COLOR_PAIR(1));
+  wattron(wnd, A_BOLD);
+  wprintw(wnd, "Enter your name...\n");
+  wgetnstr(wnd, name, MAX_NAME_LEN);
+  name[MAX_NAME_LEN] = 0;
+  wprintw(wnd, "Hello, %s!", name);
+  wrefresh(wnd);
+  delwin(wnd);
+  curs_set(FALSE);
+  move(8, 4);
+  printw("Press any key to continue...");
+  refresh();
+  getch();
+  endwin();
   exit(EXIT_SUCCESS);
 }
-
-// void QuickSort(struct dirent *namelist[], int start, int finish) {
-//   int left = start;
-//   int right = finish;
-//   int middle = (start + finish) / 2;
-//   char pivot = namelist[middle]->d_name[0];  
-
-//   while (left <= right) {
-//     while (namelist[left]->d_name[0] < pivot) left++;
-//     while (namelist[right]->d_name[0] > pivot)
-
-//  right--;
-
-//     if (left <= right) {
-//       struct dirent *buff = namelist[left];
-//       namelist[left] = namelist[right];
-//       namelist[right] = buff;
-//       left++;
-//       right--;
-//     }
-//   }
-
-//   if (start < right) QuickSort(namelist, start, right);
-//   if (left < finish) QuickSort(namelist, left, finish);
-// }
