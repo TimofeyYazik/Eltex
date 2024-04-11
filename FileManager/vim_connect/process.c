20891005eef9f7bac030c70cca5c324549d6fabd
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-11 13:18:03 +0300

    dev

--- FileManager/vim_connect/process.c
+++ FileManager/vim_connect/process.c
@@ -3,13 +3,27 @@
 #include <sys/ioctl.h>
 #include <stdlib.h>
 #include <stdio.h>
+// #include <ncurses.h>
+#include <termios.h>
 
+void saveTerminalSettings(struct termios *original_termios) {
+    tcgetattr(STDIN_FILENO, original_termios);
+}
+
+void restoreTerminalSettings(const struct termios *original_termios) {
+    tcsetattr(STDIN_FILENO, TCSANOW, original_termios);
+}
 
 void VimCall(char *name) {
+    struct termios original_termios;
+    saveTerminalSettings(&original_termios);
+
     int status = 0;
     pid_t pid_vim = fork();
     if (!pid_vim) {
         execl("/usr/bin/vim", "vim", name, NULL);
-    } 
-    waitpid(pid_vim, &status, 0);
+    }
+    pid_vim = wait(&status);
+
+    restoreTerminalSettings(&original_termios);
 }
