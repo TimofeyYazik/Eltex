87bbb00da99386c8eef17d37b3bba5951de16a6e
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-11 13:24:36 +0300

    dev

--- FileManager/vim_connect/process.c
+++ FileManager/vim_connect/process.c
@@ -5,23 +5,18 @@
 #include <stdio.h>
 #include <ncurses.h>
 // #include <termios.h>
-
-void saveNcursesSettings(SCREEN **original_screen) {
-    def_prog_mode();  // Сохранить настройки экрана ncurses
-    refresh();  // Обновить экран, чтобы изменения вступили в силу
-    *original_screen = newterm(NULL, stdout, stdin);  // Создать новый экран для восстановления
+void saveNcursesSettings(WINDOW **original_window) {
+    *original_window = dupwin(stdscr);  // Дублировать текущее окно
 }
 
-void restoreNcursesSettings(SCREEN **original_screen) {
-    endwin();  // Завершить работу с текущим экраном
-    set_term(*original_screen);  // Восстановить сохраненные настройки экрана
-    reset_prog_mode();  // Восстановить сохраненное состояние экрана
-    refresh();  // Обновить экран
+void restoreNcursesSettings(WINDOW **original_window) {
+    wrefresh(*original_window);  // Обновить дублированное окно
+    delwin(*original_window);    // Удалить дублированное окно
 }
 
 void VimCall(char *name) {
-    SCREEN *original_screen = NULL;
-    saveNcursesSettings(&original_screen);
+    WINDOW *original_window = NULL;
+    saveNcursesSettings(&original_window);
 
     int status = 0;
     pid_t pid_vim = fork();
@@ -30,5 +25,5 @@ void VimCall(char *name) {
     }
     pid_vim = wait(&status);
 
-    restoreNcursesSettings(&original_screen);
+    restoreNcursesSettings(&original_window);
 }
