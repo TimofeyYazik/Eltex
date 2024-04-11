878391d2a8537599c06698dbf38a8295c8ac15f0
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-11 17:04:26 +0300

    ready

--- FileManager/handler_touch/handler.h
+++ FileManager/handler_touch/handler.h
@@ -2,6 +2,7 @@
 #define FILE_MANAGER_HANDLER_TOUCH_HANDLER_H_
 
 #include "../castom_type.h"
+#include <ncurses.h>
 
 void TouchHandler( DirList *left_dir, WINDOW **left_window, WINDOW **right_window,
                   DirList *right_dir, SwitchDir *sw_dr);
