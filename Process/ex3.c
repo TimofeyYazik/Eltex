67564029745ec20056a67a3351e843ea18a2267b
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-20 16:28:19 +0300

    dev

--- Process/ex3.c
+++ Process/ex3.c
@@ -12,14 +12,12 @@ typedef struct {
 
 void safe_input(input_user *data) {
   memset(data->arr, 0, data->len_str);
-  data->len_str - 0;
+  data->len_str = 0;
   char ch = 0;
   while ((ch = getchar()) != '\n') {
     if (data->len_str == data->size) {
       data->size *= 2;
-      data->arr = realloc(
-          data->arr,
-          data->size);  // Используем realloc для изменения размера буфера
+      data->arr = realloc(data->arr, data->size);  
     }
     data->arr[data->len_str] = ch;
     data->len_str++;
