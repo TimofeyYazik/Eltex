e3070002263fc1dc53b47e8a6bd6e5c15c8ec37b
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-23 10:33:24 +0300

    dev

--- QueueMessage/Posix/server.c
+++ QueueMessage/Posix/server.c
@@ -1,29 +1,26 @@
-#include <fcntl.h>           /* For O_* constants */
-#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <mqueue.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <sys/types.h>
-#include <unistd.h>
-
 
 #define MAX_PATH_LENGTH 256
 
 int main() {
-  char path[MAX_PATH_LENGTH];
-  char path_client[MAX_PATH_LENGTH];
+    char path[MAX_PATH_LENGTH] = "/Users/user/Eltex/QueueMessage/Posix/server";
+    char path_client[MAX_PATH_LENGTH] = "/Users/user/Eltex/QueueMessage/Posix/client";
 
-  sprintf(path, "/Users/user/Eltex/QueueMessage/Posix/server_%d", getpid());
-  sprintf(path_client, "/Users/user/Eltex/QueueMessage/Posix/client_%d", getpid());
-  char message[20] = "hello";
-  int priority = 1;
-  mqd_t mqdes_cl = mq_open(path_client, O_CREAT | O_RDONLY);
-  mqd_t mqdes = mq_open(path, O_CREAT | O_WRONLY);
-  mq_send(mqdes, message, 20, 1);
-  mq_receive(mqdes_cl, message, 20, &priority);
-  printf("%s", message);
-  mq_close(mqdes);
-  mq_close(mqdes_cl);
-  exit(EXIT_SUCCESS);
+    char message[20] = "hello";
+    int priority = 0;
+    mqd_t mqdes_cl = mq_open(path_client, O_CREAT | O_RDONLY, 0666, NULL);
+    mqd_t mqdes = mq_open(path, O_CREAT | O_WRONLY, 0666, NULL);
+    mq_send(mqdes, message, 20, 1);
+    mq_receive(mqdes_cl, message, 20, &priority);
+    printf("%s", message);
+    mq_close(mqdes);
+    mq_close(mqdes_cl);
+    mq_unlink(path);
+    mq_unlink(path_client);
+    exit(EXIT_SUCCESS);
 }
