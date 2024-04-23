9c53a5e8b6815994415710e004a9ae492099ece2
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-23 10:55:34 +0300

    dev

--- QueueMessage/Posix/client.c
+++ QueueMessage/Posix/client.c
@@ -11,17 +11,18 @@
 #define MAX_PATH_LENGTH 256
 
 int main() {
-  char path[MAX_PATH_LENGTH] = "/Users/user/Eltex/QueueMessage/Posix/server";
-  char path_client[MAX_PATH_LENGTH] = "/Users/user/Eltex/QueueMessage/Posix/client";
+  char path[MAX_PATH_LENGTH] = "/server";
+  char path_client[MAX_PATH_LENGTH] = "/client";
   int priority = 0;
   char message[20];
-  mqd_t mqdes_cl = mq_open(path_client, O_CREAT | O_WRONLY, 0666, NULL);
+  // mqd_t mqdes_cl = mq_open(path_client, O_CREAT | O_WRONLY);
   mqd_t mqdes = mq_open(path, O_CREAT | O_RDONLY, 0666, NULL);
   mq_receive(mqdes, message, 20, &priority);
   printf("%s\n", message);
-  strcpy(message, "hi   ");
-  mq_send(mqdes_cl, message, 20, 1);
+  // strcpy(message, "hi  ");
+  // mq_send(mqdes_cl, message, 20, 1);
   mq_close(mqdes);
-  mq_close(mqdes_cl);
+  mq_unlink(path);
+  // mq_close(mqdes_cl);
   exit(EXIT_SUCCESS);
 }
--- QueueMessage/Posix/server.c
+++ QueueMessage/Posix/server.c
@@ -9,20 +9,19 @@
 #define MAX_PATH_LENGTH 256
 
 int main() {
-    char path[MAX_PATH_LENGTH] = "/Users/user/Eltex/QueueMessage/Posix/server";
-    char path_client[MAX_PATH_LENGTH] = "/Users/user/Eltex/QueueMessage/Posix/client";
-
+    char path[MAX_PATH_LENGTH] = "/server";
+    char path_client[MAX_PATH_LENGTH] = "/client";
     char message[20] = "hello";
-    int priority = 0;
-    mqd_t mqdes_cl = mq_open(path_client, O_CREAT | O_RDONLY, 0666, NULL);
+    // int priority = 0;
+    // mqd_t mqdes_cl = mq_open(path_client, O_CREAT | O_RDONLY, 0666, NULL);
     mqd_t mqdes = mq_open(path, O_CREAT | O_WRONLY, 0666, NULL);
     mq_send(mqdes, message, 20, 1);
     sleep(5);
-    mq_receive(mqdes_cl, message, 20, &priority);
-    printf("%s\n", message);
+    // mq_receive(mqdes_cl, message, 20, &priority);
+    // printf("%s\n", message);
     mq_close(mqdes);
-    mq_close(mqdes_cl);
-    mq_unlink(path);
-    mq_unlink(path_client);
+    // mq_close(mqdes_cl);
+    // mq_unlink(path);
+    // mq_unlink(path_client);
     exit(EXIT_SUCCESS);
 }
