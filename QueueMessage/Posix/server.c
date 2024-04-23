843d5867f056f87962026c599d512eae67ad2236
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-23 10:01:21 +0300

    dev

--- QueueMessage/Posix/server.c
+++ QueueMessage/Posix/server.c
@@ -13,7 +13,7 @@ int main(){
   int priority = 1;
   struct timespec delay = {1, 0};
   mqd_t mqdes = mq_open(path, O_CREAT | O_WRONLY | O_RDONLY);
-  mq_send(mqdes, &message, 20, 1);
+  mq_send(mqdes, message, 20, 1);
   ssize_t k = 0;
   while (!k) {
     k = mq_timedreceive(mqdes, message, 20, &priority, &delay);
