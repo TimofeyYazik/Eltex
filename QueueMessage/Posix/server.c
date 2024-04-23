470fb0f23385e6e7f66f074b3955bead97d64ce7
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-23 10:19:40 +0300

    dev

--- QueueMessage/Posix/server.c
+++ QueueMessage/Posix/server.c
@@ -12,11 +12,10 @@ char path_client[256] = "/Users/user/Eltex/QueueMessage/Posix/client";
 int main(){
   char message[20] = "hello";
   int priority = 1;
-  struct timespec delay = {1, 0};
   mqd_t mqdes_cl = mq_open(path_client, O_CREAT | O_RDONLY);
   mqd_t mqdes = mq_open(path, O_CREAT | O_WRONLY);
   mq_send(mqdes, message, 20, 1);
-  mq_timedreceive(mqdes_cl, message, 20, &priority, &delay);
+  mq_receive(mqdes_cl, message, 20, &priority);
   printf("%s", message);
   mq_close(mqdes);
   mq_close(mqdes_cl);
