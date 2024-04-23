888da60868964d52ddecf8e74dc2a9936b43d0dd
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-23 10:38:06 +0300

    dev

--- QueueMessage/Posix/server.c
+++ QueueMessage/Posix/server.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
 
 #define MAX_PATH_LENGTH 256
 
@@ -16,8 +17,9 @@ int main() {
     mqd_t mqdes_cl = mq_open(path_client, O_CREAT | O_RDONLY, 0666, NULL);
     mqd_t mqdes = mq_open(path, O_CREAT | O_WRONLY, 0666, NULL);
     mq_send(mqdes, message, 20, 1);
+    sleep(3);
     mq_receive(mqdes_cl, message, 20, &priority);
-    printf("%s", message);
+    printf("%s\n", message);
     mq_close(mqdes);
     mq_close(mqdes_cl);
     mq_unlink(path);
