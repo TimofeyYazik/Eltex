056cc830d53553f0493b00f31e98f517acda716e
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-23 11:11:53 +0300

    dev

--- QueueMessage/Posix/client.c
+++ QueueMessage/Posix/client.c
@@ -6,29 +6,34 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-
 #define MAX_PATH_LENGTH 256
 
 int main() {
     char path[MAX_PATH_LENGTH] = "/server";
     char message[20];
 
-    mqd_t mqdes = mq_open(path, O_RDWR);
+    mqd_t mqdes = mq_open(path, O_CREAT | O_RDONLY, 0666, NULL);
     if (mqdes == -1) {
         perror("mq_open");
         exit(EXIT_FAILURE);
     }
 
     // Получаем сообщение из очереди
-    unsigned int priority;
-    mq_receive(mqdes, message, 20, &priority);
+    ssize_t bytes_read = mq_receive(mqdes, message, 20, NULL);
+    if (bytes_read == -1) {
+        perror("mq_receive");
+        exit(EXIT_FAILURE);
+    }
+
+    // Выводим полученное сообщение
+    printf("%s\n", message);
 
     // Закрываем очередь
     mq_close(mqdes);
 
-    // Выводим полученное сообщение
-    printf("Received message: %s\n", message);
+    // Удаляем очередь
+    mq_unlink(path);
 
     exit(EXIT_SUCCESS);
 }
+
