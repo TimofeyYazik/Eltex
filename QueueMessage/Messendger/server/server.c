9a46b6eb1d5e6db3f49dcbaea34e639e75b6e174
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-26 10:37:55 +0300

    dev

--- QueueMessage/Messendger/server/server.c
+++ QueueMessage/Messendger/server/server.c
@@ -113,9 +113,9 @@ int main(){
   list.len = 0;
   list.size = 10;
   list.name = malloc(sizeof(char*) * list.size);
-  pthread_create(&thread_register, &list, ThreadRegisterClient, NULL);
-  pthread_create(&thread_send, &list, ThreadSendClient, NULL);
-  pthread_create(&thread_receive, &list, ThreadReceiveClient, NULL);
+  pthread_create(&thread_register, (void *)&list, ThreadRegisterClient, NULL);
+  pthread_create(&thread_send, (void *)&list, ThreadSendClient, NULL);
+  pthread_create(&thread_receive, (void *)&list, ThreadReceiveClient, NULL);
 
   pthread_join(thread_send, NULL);
   pthread_join(thread_receive, NULL);
