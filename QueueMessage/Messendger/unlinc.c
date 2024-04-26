4917cb7aab34afd7b2bfb463dd83ade82166ccd9
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-26 16:27:02 +0300

    dev

--- QueueMessage/Messendger/unlinc.c
+++ QueueMessage/Messendger/unlinc.c
@@ -1,10 +1,8 @@
 #include <mqueue.h>
 
 int main(){
-  mq_unlink("/lp");
-  mq_unlink("/kl");
+  mq_unlink("/nigger");
+  mq_unlink("/user");
   mq_unlink("/hi");
-  mq_unlink("/j");
-  mq_unlink("/l");
   return 0;
 }
