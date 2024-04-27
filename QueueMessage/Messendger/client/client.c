e7bbaf6f04e358dd7debc850c42cb328859c078e
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-27 08:16:16 +0300

    dev

--- QueueMessage/Messendger/client/client.c
+++ QueueMessage/Messendger/client/client.c
@@ -95,7 +95,8 @@ int main(){
   mq_unlink("/nigger");
   mq_unlink("/user");
   mq_unlink("/hi");
-  
+  mq_unlink(NAME_QUEUE_SERVER);
+  mq_unlink(NAME_QUEUE_REGISTER);
   pthread_t thread_send;
   pthread_t thread_receive;
   Register();
