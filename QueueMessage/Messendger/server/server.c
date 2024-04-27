be0a284a8be875214cd9405e3d50478937a4def3
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-27 09:37:47 +0300

    dev

--- QueueMessage/Messendger/server/server.c
+++ QueueMessage/Messendger/server/server.c
@@ -168,7 +168,8 @@ void *ThreadStop(void *arg){
   fprintf(stderr, "ThreadStop start\n");
   while (stop_server)
   {
-    scanf("%d", &stop_server);
+    scanf("%c", &stop_server);
+    printf("stop_server = %d\n", stop_server);
   } 
 }
 
