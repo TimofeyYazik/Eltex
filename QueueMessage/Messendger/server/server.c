9acf9e9fe8c7c6abc14bdf45b3b9a4fa077a0ef4
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-04-28 07:00:52 +0300

    dev

--- QueueMessage/Messendger/server/server.c
+++ QueueMessage/Messendger/server/server.c
@@ -88,17 +88,18 @@ void *ThreadReceiveClient(void *arg){
     exit(EXIT_FAILURE);
   }
   while(stop_server) {
-      mq_receive(ds_queue_server, (char*)&msg_buf, sizeof(Message), NULL);
-      MsgCopy(&storage.msg[storage.len], &msg_buf);
-      fprintf(stderr ,"ThreadReceiveClient check: %s\n", storage.msg[storage.len].text);
-      storage.len++;
-      if (storage.len == storage.size) {
-        storage.size = storage.size * 2 - (storage.size / 2);
-        storage.msg = realloc(storage.msg, sizeof(Message) * storage.size);
-      }
-      usleep(10000);
-      memset(msg_buf.text, 0, sizeof(msg_buf.text));
-      memset(msg_buf.name, 0, sizeof(msg_buf.name));
+    mq_receive(ds_queue_server, (char*)&msg_buf, sizeof(Message), NULL);
+    if(msg_buf.name[0] == 0) break;
+    MsgCopy(&storage.msg[storage.len], &msg_buf);
+    fprintf(stderr ,"ThreadReceiveClient check: %s\n", storage.msg[storage.len].text);
+    storage.len++;
+    if (storage.len == storage.size) {
+      storage.size = storage.size * 2 - (storage.size / 2);
+      storage.msg = realloc(storage.msg, sizeof(Message) * storage.size);
+    }
+    usleep(10000);
+    memset(msg_buf.text, 0, sizeof(msg_buf.text));
+    memset(msg_buf.name, 0, sizeof(msg_buf.name));
   }
   mq_close(ds_queue_server);
   mq_unlink(NAME_QUEUE_SERVER);
