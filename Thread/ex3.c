dfa4682208cac54b2327ef5018be4d6f34a29433
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-05-02 08:20:58 +0300

    dev

--- Thread/ex3.c
+++ Thread/ex3.c
@@ -9,81 +9,65 @@
 #define NUM_CUSTOMERS 3
 #define RESTOCK_PACKAGE_SIZE 5000
 
-pthread_t id_provider;
-char shop_close[NUM_SHOPS] = {0};
-int shops[NUM_SHOPS] = {0};
-pthread_mutex_t mutex_shops = PTHREAD_MUTEX_INITIALIZER; 
+typedef struct{
+  int id;
+  int package;
+} Client;
 
-void *customers_buy(void *argc){
-  int id = pthread_self();
-  int *customers = (int *)argc;
-  
-  while(*customers != 0){
-    int i = 0;  
-    printf("id thread = %d\n", id);
-    printf("need %d\n", *customers);
-    
-    pthread_mutex_lock(&mutex_shops);
-    int shop_index = -1;
-    for (; i < 5; i++) {
-      if (shop_close[i] == 0) {
-        shop_close[i] = 1; // Mark the shop as closed
-        shop_index = i;
-        break;
+typedef struct{
+  int product;
+  int is_closed;
+  pthread_mutex_t mutex;
+} Shop;
+
+
+Client customers[NUM_CUSTOMERS];
+Shop shops[NUM_SHOPS];
+
+void *CustomersBuy(void *argc){
+  Client *client = (Client *) argc;
+  int prev_product = 0;
+  int i = 0;
+  while (client->package > 0){
+   for(i = 0; i < NUM_SHOPS; i++){
+     if(shops[i].is_closed || shops[i].product == 0) continue;
+     pthread_mutex_lock(&shops[i].mutex);
+     shops[i].is_closed = 1;
+     if(client->package < shops[i].product){
+        shops[i].product -= client->package;
+        client->package = 0; 
+      } else {  
+        client->package -= shops[i].product;
+        prev_product = shops[i].product;
+        shops[i].product = 0;
       }
-    }
-    pthread_mutex_unlock(&mutex_shops);
-    if (shop_index == -1) {
-      printf("All shops are busy, waiting...\n");
-      sleep(2);
-      continue;
-    }
-    
-    if (*customers >= shops[shop_index]) {
-      *customers -= shops[shop_index];
-      shops[shop_index] = 0;
-    } else {
-      shops[shop_index] -= *customers;
-      *customers = 0;
-    }
-    
-    shop_close[shop_index] = 0;
-    sleep(2);
+     shops[i].is_closed = 0;
+     pthread_mutex_unlock(&shops[i].mutex);
+     printf("Customer %d bought %d from shop %d\n", client->id, prev_product, i);
+     printf("Customer %d has %d package\n", client->id, client->package);
+   } 
+   if(i == NUM_SHOPS) printf("Customer %d is out of shops\n", client->id);
+   sleep(2);
   }
-  
-  return NULL; 
+  printf("Customer %d is out of packages\n", client->id);
+  return NULL;
 }
 
-void *provider(void *argc){
-  int package = RESTOCK_PACKAGE_SIZE;
-  id_provider = pthread_self();
-  
-  while(1){
-    printf("id provider thread = %d\n", id_provider);
-    printf("+5000\n");
-    
-    pthread_mutex_lock(&mutex_shops);
-    int shop_index = -1;
-    for (int i = 0; i < 5; i++) {
-      if (shop_close[i] == 0) {
-        shop_index = i;
-        shop_close[i] = 1;
-        break;
-      }
+void *Provider(void *argc){
+  int i = 0;
+  while (1) {
+    for(i = 0; i < NUM_SHOPS; i++){
+      if(shops[i].is_closed) continue;
+      pthread_mutex_lock(&shops[i].mutex);
+      shops[i].is_closed = 1;
+      shops[i].product += RESTOCK_PACKAGE_SIZE;
+      shops[i].is_closed = 0;
+      pthread_mutex_unlock(&shops[i].mutex);
     }
-    pthread_mutex_unlock(&mutex_shops);
-    if (shop_index == -1) {
-      printf("All shops are busy for restocking, waiting...\n");
-      sleep(1);
-      continue;
-    }
-    
-    shop_close[shop_index] = 0;
-    shops[shop_index] += package; // Restock the shop
+    if(i == NUM_SHOPS) printf("Provider is out of shops\n"); 
+    else printf("Provider restocked shop %d\n", i);
     sleep(1);
   }
-  
-  return NULL;
 }
 
 int main(){
@@ -93,21 +77,25 @@ int main(){
   int lower = 5000, upper = 10000;
   
   for(int i = 0; i < NUM_SHOPS; i++){
-    shops[i] = (rand() % (upper - lower + 1)) + lower;
+    shops[i].product = (rand() % (upper - lower + 1)) + lower;
+    shops[i].is_closed = 0;
   }
-  
-  int customers_arr[NUM_CUSTOMERS] = {100000, 100000, 100000}; 
-  
   for(int i = 0; i < NUM_CUSTOMERS; i++){
-    pthread_create(&customers_pthread[i], NULL, customers_buy, &customers_arr[i]);
+    customers[i].id = i;
+    customers[i].package = 100000;
+  }
+  for(int i = 0; i < NUM_CUSTOMERS; i++){
+    pthread_create(&customers_pthread[i], NULL, CustomersBuy, &customers[i]);
   }
   
-  pthread_create(&provider_pthread, NULL, provider, NULL);
+  pthread_create(&provider_pthread, NULL, Provider, NULL);
   
   for(int i = 0; i < NUM_CUSTOMERS; i++){
     pthread_join(customers_pthread[i], NULL);
   }
-  
-  pthread_cancel(id_provider);
+  pthread_cancel(provider_pthread);
+  for(int i = 0; i < NUM_SHOPS; i++){
+    pthread_mutex_destroy(&shops[i].mutex);
+  }
   exit(EXIT_SUCCESS);
 }
