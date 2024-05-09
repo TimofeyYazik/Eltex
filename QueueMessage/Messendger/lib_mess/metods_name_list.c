0e4af3027cdb103aa7a0e2c94b390b5fb05d3ed5
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-05-09 17:42:18 +0300

    dev

--- QueueMessage/Messendger/lib_mess/metods_name_list.c
+++ QueueMessage/Messendger/lib_mess/metods_name_list.c
@@ -1,7 +1,7 @@
 #include "lib_mess.h"
 
 void AddNameList(NameList *src, char *name){
-  if(src->name[src->len] == NULL) src->name[src->len] = malloc(char * MAX_NAME_LEN);
+  if(src->name[src->len] == NULL) src->name[src->len] = malloc(sizeof(char) * MAX_NAME_LEN);
   strcpy(src->name[src->len], name);
   src->len++;
   if(src->len == src->size) {
