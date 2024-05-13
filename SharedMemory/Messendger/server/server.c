ef64b35641df10f5b266ad673e5633c39a8e3bdf
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-05-13 16:31:00 +0300

    dev

--- SharedMemory/Messendger/server/server.c
+++ SharedMemory/Messendger/server/server.c
@@ -31,7 +31,7 @@ int main(){
   while (1)
   {
     scanf("%d", &stop_server);
-    printf("%s\n", cont_ptr->list.name[cont_ptr->list.len]);
+    printf("%s\n", cont_ptr->list.name[0]);
     if(stop_server == 0) break;
   }
   munmap(cont_ptr, sizeof(Controller));
