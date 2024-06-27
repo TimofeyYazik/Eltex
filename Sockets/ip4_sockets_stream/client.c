4a3bccb813029b607d19cedbb5a7c0073ef727f5
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-06-27 14:30:25 +0300

    dev

---
+++ Sockets/ip4_sockets_stream/client.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+
+
+#define SA struct sockaddr
+#define PORT 8084
+#define SIZE_BUFF 128
+#define handle_error(msg) \
+           do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+int main(){
+  int cfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(cfd < 0){
+    handle_error("sockets");
+  }
+  struct sockaddr_in serv_sock;
+  memset(&serv_sock, 0 , sizeof(serv_sock));  
+  serv_sock.sin_family = AF_INET;
+  serv_sock.sin_addr.s_addr = INADDR_ANY;
+  serv_sock.sin_port = htons(PORT);
+  if(connect(cfd, (SA*) &serv_sock, sizeof(serv_sock)) < 0){
+    handle_error("connect");
+  }
+  char buff[SIZE_BUFF];
+  strcpy(buff, "hello");
+  send(cfd, buff, SIZE_BUFF, 0);
+  recv(cfd, buff, SIZE_BUFF, 0);
+  printf("%s\n", buff);
+  send(cfd, buff, SIZE_BUFF, 0);
+  close(cfd);
+  exit(EXIT_SUCCESS);
+}
---
+++ Sockets/ip4_sockets_stream/server.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
+
+
+#define SA struct sockaddr
+#define PORT 8084
+#define SIZE_BUFF 128
+#define handle_error(msg) \
+           do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+int main(){
+  int sfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sfd < 0){
+    handle_error("sockets");
+  }
+  struct sockaddr_in serv_sock, client_sock;
+  memset(&serv_sock, 0 , sizeof(serv_sock));  
+  memset(&client_sock, 0 , sizeof(client_sock));
+  serv_sock.sin_family = AF_INET;
+  serv_sock.sin_addr.s_addr = INADDR_ANY;
+  serv_sock.sin_port = htons(PORT);
+  if(bind(sfd, (struct sockaddr*)&serv_sock, sizeof(serv_sock)) < 0){
+      handle_error("bind");
+  }
+  socklen_t serv_len = sizeof(serv_sock);
+  if(listen(sfd, 5) < 0){
+    handle_error("listen");
+  }
+  int activ_fd = accept(sfd, (SA*) &serv_sock, &serv_len);
+  char buff[SIZE_BUFF];
+  recv(activ_fd, buff, SIZE_BUFF, 0);
+  printf("%s\n", buff);
+  strcpy(buff, "hi");
+  send(activ_fd, buff, SIZE_BUFF, 0);
+  recv(activ_fd, buff, SIZE_BUFF, 0);
+  close(activ_fd);
+  close(sfd);
+  exit(EXIT_SUCCESS);
+}
