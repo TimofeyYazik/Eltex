9df46ec9b51eace866a9efa63a72838a8d0633e3
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-06-28 14:12:56 +0300

    dev

---
+++ Servers/thread_server/client.c
@@ -0,0 +1,58 @@
+#include <signal.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/signal.h>
+
+#include "double_list/list.h"
+
+#define SA struct sockaddr
+#define PORT 6666
+#define SIZE_BUFF 8
+#define IP_ADDRES "127.0.0.1"
+#define handler_error(text) \
+          do { perror(text); exit(EXIT_FAILURE); } while(0);
+
+
+
+int main(){
+  int ip_addres = 0;
+  inet_pton(AF_INET, IP_ADDRES, &ip_addres);
+  int cfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(cfd == -1){
+    handler_error("socket");
+  }
+  struct sockaddr_in server_connect;
+  server_connect.sin_family = AF_INET;
+  server_connect.sin_addr.s_addr = ip_addres;
+  server_connect.sin_port = htons(PORT);
+  char buff[SIZE_BUFF];
+  if(connect(cfd, (SA*)&server_connect, sizeof(server_connect)) == -1){
+    handler_error("ne vezet");
+  }
+  char time_str[80] = {0};
+  time_t time_self;
+  struct tm * timeinfo;
+  while(1){
+    scanf("%7c", buff);
+    if(!strcmp(buff, "exit")){
+      send(cfd, buff, SIZE_BUFF, 0);
+      break;
+    }    
+    if(!strcmp(buff, "time")){
+      send(cfd, buff, SIZE_BUFF, 0);
+      recv(cfd, &time_self, SIZE_BUFF, 0);
+      timeinfo = localtime(&time_self);
+      strftime(time_str, 80, "%Y-%m-%d %H:%M:%S", timeinfo);
+      printf("%s\n", time_str);
+    }
+  }
+  exit(EXIT_SUCCESS);
+}
---
+++ Servers/thread_server/server.c
@@ -0,0 +1,87 @@
+#include <signal.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/signal.h>
+
+#include "double_list/list.h"
+
+#define SA struct sockaddr
+#define PORT 6666
+#define SIZE_BUFF 8
+#define IP_ADDRES "127.0.0.1"
+#define handler_error(text) \
+          do { perror(text); exit(EXIT_FAILURE); } while(0);
+
+
+void *ChildServer(void *fd){
+  int *active_fd = (int *)fd;
+  time_t time_now;
+  char buff[SIZE_BUFF];
+  while (1) {
+    recv(*active_fd, buff, SIZE_BUFF, 0);
+    if(!strcmp(buff, "exit")){
+      break;
+    } else {
+      time(&time_now);
+      send(*active_fd, (void *)time, SIZE_BUFF, 0);
+    }  
+  }  
+  return NULL;
+}
+
+int stop = 1;
+
+void HandlerUserSignal(int a){
+  stop = 0;
+}
+
+
+int main(){
+  struct sigaction sig;
+  sig.sa_flags = 0;
+  sig.sa_mask |= SIGUSR1;
+  sig.__sigaction_u.__sa_handler = HandlerUserSignal;
+  sigaction(SIGUSR1, &sig, NULL);
+  int len_treads_arr = 100;
+  pthread_t *arr_treads = malloc(len_treads_arr * sizeof(pthread_t));
+  int ip_addres = 0;
+  inet_pton(AF_INET, IP_ADDRES, &ip_addres);
+  int main_sfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(main_sfd == -1){
+    handler_error("socket");
+  }
+  struct sockaddr_in server_settings;
+  server_settings.sin_family = AF_INET;
+  server_settings.sin_addr.s_addr = ip_addres;
+  server_settings.sin_port = htons(PORT);
+
+  if(bind(main_sfd,(SA *)&server_settings, sizeof(server_settings)) == -1){
+    handler_error("bind");
+  }
+  if(listen(main_sfd, 10) == -1){
+    handler_error("listen");
+  }
+  socklen_t len = sizeof(server_settings);
+  int counter = 0;
+  while(stop){
+    int active_fd = accept(main_sfd, (SA *)&server_settings, &len);
+    pthread_create(&arr_treads[counter], NULL, ChildServer, (void *)&active_fd); 
+    counter++;
+    if(counter == len_treads_arr){
+      len_treads_arr *= 2;
+      arr_treads = realloc(arr_treads, len_treads_arr);
+    }
+  }
+  for(int i = 0; i < counter; i++){
+    pthread_join(arr_treads[i], NULL);
+  }
+  exit(EXIT_SUCCESS);
+}
