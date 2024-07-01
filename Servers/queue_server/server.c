bb7c60b08f12553852b5c43a0680002464c474af
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-07-01 09:50:53 +0300

    dev

--- Servers/queue_server/server.c
+++ Servers/queue_server/server.c
@@ -111,7 +111,7 @@ int main() {
   if (bind(main_sfd, (SA *)&server_settings, sizeof(server_settings)) == -1) {
     handler_error("bind");
   }
-  if (listen(main_sfd, 100) == -1) {
+  if (listen(main_sfd, 1000) == -1) {
     handler_error("listen");
   }
 
