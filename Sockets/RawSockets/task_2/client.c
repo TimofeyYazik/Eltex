d95186cdbfcb345acbe2dd03172e09bf843bf44b
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-07-02 07:48:12 +0300

    dev

--- Sockets/RawSockets/task_2/client.c
+++ Sockets/RawSockets/task_2/client.c
@@ -19,16 +19,6 @@
 do{ perror(text); exit(EXIT_FAILURE); } while(1);
 
 
-void *ThreadStop(void *stop_p){
-  int *stop = stop_p;
-  while (*stop) {
-    if(scanf("%d", stop) != 1){
-      *stop = 0;
-    }
-  }
-  return NULL;
-}
-
 int main(){
   int cfd = 0;
   int stop = 1;
@@ -45,20 +35,20 @@ int main(){
   server_endpoint.sin_port = htons(PORT);
   socklen_t size = sizeof(server_endpoint);
   pthread_t stop_client = 0;
-  pthread_create(&stop_client, NULL, ThreadStop, &stop);
   printf("PRESS 0 (ZERO) CLIENT STOP\n");
 
   int16_t *point_head = &buff[0];
   *point_head = htons(SOURCE_PORT);
-  point_head += 2;
+  point_head++;
   *point_head = htons(PORT);
-  point_head += 2;
+  point_head++;
   *point_head = SIZE_BUFF;
-  point_head += 2;
+  point_head++;
   *point_head = 0;
   
   while(stop){
     scanf("%119s", buff + 8);
+    if(!strcmp(buff + 8, "exit")) break;
     sendto(cfd, buff, SIZE_BUFF, 0, (SA*)&server_endpoint, size);
     recvfrom(cfd, buff + 8, 120, 0, (SA*)&server_endpoint, &size);
     printf("%s\n", buff);
