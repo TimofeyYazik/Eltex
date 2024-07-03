04d61fa1e9638d221627d083340a66cd6ddbb080
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-07-03 17:22:44 +0300

    dev

--- Sockets/RawSockets/task_2/client.c
+++ Sockets/RawSockets/task_2/client.c
@@ -77,9 +77,10 @@ int main() {
         printf("messege send!\n");
         while (1) {
             recvfrom(cfd, buff_recv, SIZE_BUFF, 0, (SA*)&server_recv, &serv);
+            printf("%s\n", buff+28);
             udph = (struct udphdr *)(buff_recv + sizeof(struct iphdr));
             if(udph->dest == htons(SOURCE_PORT)){
-               printf("%s", buff_recv + 28);
+               printf("%s\n", buff_recv + 28);
                break;
             }
         }
