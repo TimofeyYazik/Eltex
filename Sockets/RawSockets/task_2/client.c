29ad52168647abeaeff5fbf63a04b8cbc477a724
Юзяк Тимофей <olesiytrupoedova@gmail.com>
2024-07-03 15:40:38 +0300

    dev

--- Sockets/RawSockets/task_2/client.c
+++ Sockets/RawSockets/task_2/client.c
@@ -13,7 +13,8 @@
 #define PORT 6666
 #define SOURCE_PORT 7777
 #define IP_ADDRES "127.0.0.1"
-#define SIZE_BUFF 128
+#define SIZE_BUFF_SEND 18
+#define SIZE_BUFF_RECV 38
 #define SA struct sockaddr
 #define handler_error(text) \
 do{ perror(text); exit(EXIT_FAILURE); } while(1);
@@ -22,7 +23,8 @@ do{ perror(text); exit(EXIT_FAILURE); } while(1);
 int main(){
   int cfd = 0;
   int stop = 1;
-  char buff[SIZE_BUFF] = {0};
+  char buff_send[SIZE_BUFF_SEND] = {0};
+  char buff_recv[SIZE_BUFF_RECV] = {0};
   int16_t ip_addres = 0;
   inet_pton(AF_INET, IP_ADDRES, &ip_addres);
   cfd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
@@ -37,22 +39,22 @@ int main(){
   pthread_t stop_client = 0;
   printf("PRESS 0 (ZERO) CLIENT STOP\n");
 
-  int16_t *point_head = &buff[0];
+  int16_t *point_head = &buff_send[0];
   *point_head = htons(SOURCE_PORT);
   point_head++;
   *point_head = htons(PORT);
   point_head++;
-  *point_head = SIZE_BUFF;
+  *point_head = htons(SIZE_BUFF_SEND);
   point_head++;
   *point_head = 0;
-  point_head -= 2;
   while(stop){
-    scanf("%119s", buff + 8);
-    if(!strcmp(buff + 8, "exit")) break;
-    sendto(cfd, buff, SIZE_BUFF, 0, (SA*)&server_endpoint, size);
-    recvfrom(cfd, buff, SIZE_BUFF, 0, (SA*)&server_endpoint, &size);
+    scanf("%9s", buff_send + 8);
+    if(!strcmp(buff_send + 8, "exit")) break;
+    sendto(cfd, buff_send, SIZE_BUFF_SEND, 0, (SA*)&server_endpoint, size);
+    recvfrom(cfd, buff_recv, SIZE_BUFF_RECV, 0, (SA*)&server_endpoint, &size);
+    point_head = &buff_recv[22];
     if(*point_head == htons(SOURCE_PORT))
-      printf("%s\n", buff);
+      printf("%s\n", buff_recv + 28);
   }
   pthread_join(stop_client, NULL);
   close(cfd);
